2ba5288d66953627b1610ae0e0adf3d2f6b41361
carlostule <carlos.enrique.tule@gmail.com>
2022-04-06 21:36:07 -0500

    Configure React app for deployment to GitHub Pages

---
+++ src/pages/TodoListApp.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import styles from "./styles.module.css";
+
+export const TodoListApp = () => {
+  return (
+    <div className={styles.flexContainer}>
+      <header className={styles.header}>
+        <h1>#TO-DO List App</h1>
+      </header>
+    </div>
+  );
+}
